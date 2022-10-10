f44861060f58789e355646a827176f60c49c2f95
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-10 16:06:30 +0100

    set background color root

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-   <StartGameScreen/>
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  rootScreen: {
+    backgroundColor: '#ddb52f',
+    // flex: 1,
+  }
 });
