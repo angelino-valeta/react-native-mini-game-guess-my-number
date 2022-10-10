226cadffa77a4ef6f5e0c3ed9e51e674ba8c9f81
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-10 21:06:30 +0100

    set colors

--- App.js
+++ App.js
@@ -1,49 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, ImageBackground, View } from "react-native";
+import { StyleSheet, ImageBackground, View, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
 import { useState } from "react";
+import Colors from "./constants/colors";
 
 export default function App() {
-  
-  const [userNumber, setUserNumber] = useState()
+  const [userNumber, setUserNumber] = useState();
 
-  function pickedNumberHandler(pickedNumber){
-    setUserNumber(pickedNumber)
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
   }
 
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>
-  
-  if(userNumber){
-    screen = <GameScreen/>
+  if (userNumber) {
+    screen = <GameScreen />;
   }
 
   return (
     <View>
-      <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen}>
+      <LinearGradient colors={[Colors.accent500, Colors.primary700]} style={styles.rootScreen}>
         <ImageBackground
           source={require("./assets/images/background.png")}
           resizeMode="cover"
+          style={styles.rootScreen}
           imageStyle={styles.backgroundImage}
         >
-          {screen}
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
         </ImageBackground>
       </LinearGradient>
-      <View>
-        
-      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: "#ddb52f",
     // flex: 1,
   },
   backgroundImage: {
     opacity: 0.4,
+    // backgroundColor: "#ddb52f",
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,24 @@
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
+
 function GameScreen() {
-  return(
-    <Text>Game Screen!</Text>
-  )
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <View>
+        <Text>Higher or lower</Text>
+      </View>
+      <View></View>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    // flex: 1,
+    padding: 40,
+  },
+
+});
