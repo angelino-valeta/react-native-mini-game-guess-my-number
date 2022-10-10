a0bcb21c2b02912df1656503371483d2135c9310
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-10 13:45:59 +0100

    Implement PrimaryButton

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,11 @@
 import { TextInput, Button, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View>
       <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
