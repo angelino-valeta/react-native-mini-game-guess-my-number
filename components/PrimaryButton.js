40236982b312f6877430a188e35ee99c0506e98f
Angelino Valeta <angelinovaleta@gmail.com>
2022-10-10 16:35:16 +0100

    onPress props function

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,17 +1,13 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({ children }) {
-  function pressHandler() {
-    console.log("Pressed primary button");
-  }
-
-  return (
+function PrimaryButton({ children, onPress }) {
+return (
     <View style={styles.buttonOuterContainer}>
       <Pressable 
         style={({pressed}) => pressed
          ? [styles.buttonInnerContainer, styles.pressed]
          : styles.buttonInnerContainer} 
-        onPress={pressHandler} 
+        onPress={onPress} 
         android_ripple={{ color: "#640233" }}>
         <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
